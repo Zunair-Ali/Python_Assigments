9fb21eed22a5cc8a22a986daf968051aafd1eba2
Zunair-Ali <zunairliaqat375@gmail.com>
2025-04-24 00:09:17 +0500

    Initial commit

---
+++ class_questions.py
@@ -0,0 +1,67 @@
+# Class Questions
+
+
+# Find the area of a rectangle
+length = 15
+breadth = 10
+area = length * breadth
+print("Area of Rectangle:", area)
+
+# Convert Fahrenheit to Celsius
+F = 98.6
+C = (F - 32) * 5 / 9
+print("Celsius Temperature:", round(C, 2))
+
+
+# Calculate the total marks and average of 3 subjects
+math = 87
+science = 92
+english = 78
+
+total = math + science + english
+average = total / 3
+
+print("Total Marks:", total)
+print("Average Marks:", round(average, 2))
+
+# Find the perimeter of a square
+side = 12
+perimeter = 4 * side
+print("Perimeter of Square:", perimeter)
+
+# Calculate simple interest
+P = 10000
+R = 5
+T = 2
+
+SI = (P * R * T) / 100
+print("Simple Interest:", SI)
+
+# Convert height from feet to inches
+feet = 5.5
+inches = feet * 12
+print("Height in inches:", inches)
+
+# Calculate the area of a circle
+radius = 7
+pi = 3.14
+area = pi * radius * radius
+print("Area of Circle:", area)
+
+# Convert days into hours and minutes
+days = 3
+hours = days * 24
+minutes = hours * 60
+print("Hours:", hours)
+print("Minutes:", minutes)
+
+# Find the volume of a cube
+side = 9
+volume = side ** 3
+print("Volume of Cube:", volume)
+
+# Calculate BMI (Body Mass Index)
+weight = 70
+height = 1.75
+BMI = weight / (height ** 2)
+print("BMI:", round(BMI, 2))
